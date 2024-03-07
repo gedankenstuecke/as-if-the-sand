32cdefb9f0a43c3289998db8ecccba6d086e5529
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2024-03-07 17:51:16 +0000

    add maps to about

---
+++ js/about.js
@@ -0,0 +1,9 @@
+function toggleCenter() {
+    var element = document.getElementById("image-center");
+    element.classList.toggle("image-hidden");
+}
+
+function toggleTop() {
+    var element = document.getElementById("image-top");
+    element.classList.toggle("image-hidden");
+} 
